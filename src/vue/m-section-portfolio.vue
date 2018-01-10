786ecc79946616fdd87bb6f43904745483fc1278
SalygaYaroslav <salyga.yaroslav@gmail.com>
2018-01-11 00:30:42 +0200

    group

--- src/vue/m-section-portfolio.vue
+++ src/vue/m-section-portfolio.vue
@@ -23,7 +23,7 @@
             return {
                 path: '/images/gallery/',
                 images: [
-                    'pb-portall', 'pb-p1', 'pb-p2', 'pb-web', 'pb-p3', 'pb-rup', 'rso', 'artic'
+                    'pb-portall', 'pb-web', 'pb-p1', 'pb-p2', 'pb-p3', 'pb-rup', 'rso', 'artic'
                 ]
             }
         },
